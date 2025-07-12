f89c77c20aa255e5074da5cdc0ae5c78d07c08e4
General <75862857+FFXILevelDown@users.noreply.github.com>
2025-07-12 14:21:02 -0600

    Update trust back to 30 sec resummon, update utils for blue magic barrier

--- scripts/globals/utils.lua
+++ scripts/globals/utils.lua
@@ -1329,3 +1329,20 @@ function utils.timeIsBefore(current, target)
 
     return current.hour < target.hour
 end
+
+-- returns unabsorbed damage
+---@nodiscard
+---@param target CBaseEntity
+---@param dmg integer
+---@return integer
+function utils.magicBarrier(target, dmg)
+    --handling blue magic barrier
+    if dmg > 0 then
+        local effect = target:getStatusEffect(xi.effect.MAGIC_SHIELD)
+        local dmgReduction = target:getSkillLevel(xi.skill.BLUE_MAGIC) * 2.5
+        if target:hasStatusEffect(xi.effect.MAGIC_SHIELD, 2) then
+            dmg = math.max(0, dmg - dmgReduction)
+        end
+    end
+    return dmg
+end
