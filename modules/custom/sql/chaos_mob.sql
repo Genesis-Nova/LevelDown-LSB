35c2198e394b02e7aed83c818ce57efae3a0f78b
General <thomas.urias@yahoo.com>
2025-08-22 22:49:16 -0600

    New Supreme Chaos

---
+++ modules/custom/sql/chaos_mob.sql
@@ -0,0 +1,70 @@
+-- supreme bahamut ( chaos )
+INSERT INTO mob_family_system VALUES (508, 'Chaos', 78, 'Supreme_Being', 18,'Unclassified', 6.0,40,120,90,5,5,5,5,5,5,5,5,5,5,5,3,1,0);
+INSERT INTO `mob_pools` VALUES (40019,'Chaos','Chaos',508,0x00004b0e00000000000000000000000000000000,4,8,7,240,100,1024,1,1,0,2,6198,32,100,157,0,0,1000,0,0,49990,449);
+INSERT INTO `mob_groups` VALUES (11506, 40019, 299, 'Chaos',0,128,0,1550000,9999,145,150,0);
+
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 503, 1, 255); -- impact
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 218, 1, 255); -- meteor
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 275, 1, 255); -- absorb-tp
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 178, 1, 255); -- firaga_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 496, 1, 255); -- firaja
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 183, 1, 255); -- blizzaga_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 497, 1, 255); -- blizzaja
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 193, 1, 255); -- stonega_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 499, 1, 255); -- stoneja
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 203, 1, 255); -- waterga_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 501, 1, 255); -- waterja
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 188, 1, 255); -- aeroga_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 498, 1, 255); -- aeroja
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 37, 1, 255); -- diaga_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 362, 1, 255); -- bindga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 361, 1, 255); -- blindga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 365, 1, 255); -- breakga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 360, 1, 255); -- dispelga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 880, 1, 255); -- drain_iii
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 277, 1, 255); -- dread_spikes
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 366, 1, 255); -- graviga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 356, 1, 255); -- paralyga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 359, 1, 255); -- silencega
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 357, 1, 255); -- slowga
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 257, 1, 255); -- curse
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 252, 1, 255); -- stun
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 6, 1, 255); -- cure_vi
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 52, 1, 255); -- shell_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 47, 1, 255); -- protect_v
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 106, 1, 255); -- phalanx
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 54, 1, 255); -- stoneskin
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 722, 1, 255); -- entomb
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 750, 1, 255); -- mighty gaurd
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 720, 1, 255); -- spectral_floe
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 668, 1, 255); -- magic_barrier
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 719, 1, 255); -- searing_tempest
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 728, 1, 255); -- tenebral_crush
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 721, 1, 255); -- anvil_lightning
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 725, 1, 255); -- blinding_fulgor
+INSERT INTO mob_spell_lists VALUES ('[Supreme]Chaos', 1000, 726, 1, 255); -- scouring_spate
+
+INSERT INTO mob_skills VALUES (4376, 1133, 'overrun', 1, 0.0, 10.0, 2000,1500, 4, 0, 0, 7, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4379, 1140, 'swooping_assault', 1, 0.0, 20.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4384, 2627, 'tenebrous_surge', 1, 0.0, 20.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4377, 1134, 'wing_buffer', 1, 0.0, 20.0, 2000,1500, 4, 0, 0, 7, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4380, 1135, 'wrenching_impale', 0, 0.0, 10.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4382, 1143, 'opressive_yawp', 1, 0.0, 30.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4381, 1142, 'seeping_dread', 1, 0.0, 20.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4385, 2628, 'pellucid_surge', 0, 0.0, 10.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4383, 2626, 'luminous_surge', 0, 0.0, 10.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4378, 1139, 'landfall', 1, 0.0, 10.0, 2000,1500, 4, 0, 0, 7, 0, 0, 0);
+INSERT INTO mob_skills VALUES (4386, 2629, 'black_sanctus', 1, 0.0, 15.0, 2000,1500, 4, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4376); -- Overrun
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4379); -- Swooping Assault
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4384); -- Tenebrous Surge
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4377); -- Wing Buffer
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4380); -- Wrenching Impale
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4382); -- Opressive Yawp
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4381); -- Seeping Dread
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4385); -- Pellucid Surge
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4383); -- Luminous Surge
+INSERT INTO mob_skill_lists VALUES ('[Supreme]Chaos', 49990, 4378); -- Landfall
+INSERT INTO 'mob_skill_lists' VALUES ('[Supreme]Chaos', 49990, 4386); -- Black Sanctus
+
