dbf3a2423dc81de0caab0c2e7356f8ba313de90a
General <75862857+FFXILevelDown@users.noreply.github.com>
2026-01-19 15:48:09 -0700

    Escha Ruaun Updates

---
+++ scripts/zones/Escha_RuAun/mobs/Eschan_Clionid.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Escha Ruaun
+--  Mob: Eschan_Clionid
+-- Note: PH for Warder_of_Loyalty
+-----------------------------------
+local ID = zones[xi.zone.ESCHA_RUAUN]
+-----------------------------------
+---@type TMobEntity
+local entity = {}
+
+entity.onMobDeath = function(mob, player, optParams)
+    local params =
+    {
+        immediate = true,
+        spawnPoints = { { x = mob:getXPos(), y = mob:getYPos(), z = mob:getZPos() } }
+    }
+
+    if not player:hasKeyItem(xi.ki.MOLLIFIER) then
+        xi.mob.phOnDespawn(mob, ID.mob.WARDER_OF_LOYALTY, 15, 4800, params)
+    end
+end
+
+entity.onMobDespawn = function(mob)
+
+end
+
+return entity
---
+++ scripts/zones/Escha_RuAun/mobs/Eschan_Yovra.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Escha Ruaun
+--  Mob: Eschan_Yovra
+-- Note: PH for Warder_of_Love
+-----------------------------------
+local ID = zones[xi.zone.ESCHA_RUAUN]
+-----------------------------------
+---@type TMobEntity
+local entity = {}
+
+entity.onMobDeath = function(mob, player, optParams)
+    local params =
+    {
+        immediate = true,
+        spawnPoints = { { x = mob:getXPos(), y = mob:getYPos(), z = mob:getZPos() } }
+    }
+
+    if not player:hasKeyItem(xi.ki.MOLLIFIER) then
+        xi.mob.phOnDespawn(mob, ID.mob.WARDER_OF_LOVE, 15, 4800, params)
+    end
+end
+
+entity.onMobDespawn = function(mob)
+
+end
+
+return entity
