5d626b3bd7b21e153d70e653753b81bf6811cb34
kobrac0mmander <142762651+kobrac0mmander@users.noreply.github.com>
2026-02-26 11:40:08 -0600

    Update item_Updates.sql
    Lockstyle Item fixes

--- modules/custom/sql/item_Updates.sql
+++ modules/custom/sql/item_Updates.sql
@@ -8,3 +8,60 @@ UPDATE item_basic SET stackSize = 99 WHERE itemid = 2885;
 
 --Date_Shuriken Fix
 UPDATE `item_weapon` SET `subskill`=3, `ilvl_skill`=242 WHERE `itemId`=22292 AND name = 'date_shuriken';
+
+--Lockstyle Fixes
+
+UPDATE item_basic SET type = 7 WHERE itemID = 20931 AND name = 'celestial_spear';
+UPDATE item_basic SET type = 7 WHERE itemID = 21520 AND name = 'ethereal_fists';
+UPDATE item_basic SET type = 7 WHERE itemID = 21624 AND name = 'ethereal_sword';
+UPDATE item_basic SET type = 7 WHERE itemID = 21666 AND name = 'ethereal_g._sword';
+UPDATE item_basic SET type = 7 WHERE itemID = 21713 AND name = 'ethereal_axe';
+UPDATE item_basic SET type = 7 WHERE itemID = 21771 AND name = 'ethereal_great_axe';
+UPDATE item_basic SET type = 7 WHERE itemID = 21838 AND name = 'ethereal_scythe';
+UPDATE item_basic SET type = 7 WHERE itemID = 21873 AND name = 'ethereal_spear';
+UPDATE item_basic SET type = 7 WHERE itemID = 21911 AND name = 'ethereal_katana';
+UPDATE item_basic SET type = 7 WHERE itemID = 21962 AND name = 'ethereal_tachi';
+UPDATE item_basic SET type = 7 WHERE itemID = 22053 AND name = 'ethereal_club';
+UPDATE item_basic SET type = 7 WHERE itemID = 22090 AND name = 'ethereal_staff';
+UPDATE item_basic SET type = 7 WHERE itemID = 22146 AND name = 'ethereal_bow';
+UPDATE item_basic SET type = 7 WHERE itemID = 22165 AND name = 'ethereal_gun';
+UPDATE item_basic SET type = 6 WHERE itemID = 23822 AND name = 'diamond_somen';
+UPDATE item_basic SET type = 6 WHERE itemID = 23823 AND name = 'diamond_haramaki';
+UPDATE item_basic SET type = 6 WHERE itemID = 23824 AND name = 'diamond_kote';
+UPDATE item_basic SET type = 6 WHERE itemID = 23825 AND name = 'diamond_hizayori';
+UPDATE item_basic SET type = 6 WHERE itemID = 23826 AND name = 'diamond_sune-ate';
+UPDATE item_basic SET type = 6 WHERE itemID = 23871 AND name = 'hebenus_gilet';
+UPDATE item_basic SET type = 6 WHERE itemID = 23872 AND name = 'hebenus_boxers';
+UPDATE item_basic SET type = 6 WHERE itemID = 23873 AND name = 'hebenus_top';
+UPDATE item_basic SET type = 6 WHERE itemID = 23874 AND name = 'hebenus_shorts';
+
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21520 AND name = 'ethereal_fists';
+UPDATE item_equipment SET slot = 3 WHERE itemID = 21624 AND name = 'ethereal_sword';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21771 AND name = 'ethereal_great_axe';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21838 AND name = 'ethereal_scythe';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21962 AND name = 'ethereal_tachi';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 22090 AND name = 'ethereal_staff';
+UPDATE item_equipment SET slot = 4 WHERE itemID = 22146 AND name = 'ethereal_bow';
+UPDATE item_equipment SET slot = 3 WHERE itemID = 21571 AND name = 'ethereal_dagger';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21713 AND name = 'ethereal_axe';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21666 AND name = 'ethereal_g._sword';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 21911 AND name = 'ethereal_katana';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 22053 AND name = 'ethereal_club';
+UPDATE item_equipment SET slot = 3 WHERE itemID = 21873 AND name = 'ethereal_spear';
+UPDATE item_equipment SET slot = 4 WHERE itemID = 22165 AND name = 'ethereal_gun';
+UPDATE item_equipment SET slot = 1 WHERE itemID = 20931 AND name = 'celestial_spear';
+UPDATE item_equipment SET slot = 3 WHERE itemID = 20593 AND name = 'hedron_dagger';
+
+
+--slots
+-- two handed 1
+-- shield 2
+-- main or offhand 3
+-- ranged 4
+-- ammo 8
+-- head 16
+-- body 32
+-- Hands 64
+-- Legs 128
+-- Feet 256
+
