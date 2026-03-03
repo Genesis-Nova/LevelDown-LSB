d5bb6ab86d47953436a86167f35f99d071fa53d4
General <75862857+FFXILevelDown@users.noreply.github.com>
2026-03-03 16:07:24 -0700

    Fixed mob mechanics

--- scripts/zones/Reisenjima/mobs/Arboreal_Chigoe.lua
+++ scripts/zones/Reisenjima/mobs/Arboreal_Chigoe.lua
@@ -8,6 +8,24 @@ local ID = zones[xi.zone.REISENJIMA]
 ---@type TMobEntity
 local entity = {}
 
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.ALWAYS_AGGRO, 1)
+    mob:setUntargetable(true)
+    mob:hideName(true)
+end
+
+entity.onMobEngage = function(mob, target)
+    mob:hideName(false)
+    mob:setUntargetable(false)
+    mob:setAnimationSub(2)
+end
+
+entity.onMobDisengage = function(mob)
+    mob:hideName(true)
+    mob:setUntargetable(true)
+    mob:setAnimationSub(0)
+end
+
 entity.onMobDeath = function(mob, player, optParams)
     local params =
     {
